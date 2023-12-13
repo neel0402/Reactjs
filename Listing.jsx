a22515fc717f5bc669d6209a3bf3f171a0f6e4a3
neel0402 <122443158+neel0402@users.noreply.github.com>
2023-12-13 11:33:07 +0530

    Add files via upload

---
+++ Listing.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ListView({ items }) {
+  return (
+    <ul
+      style={{
+        color: "white",
+        listStyle: "none",
+      }}
+    >
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default ListView;
